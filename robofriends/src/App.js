084df050baca3b2d37ae21a4e3a60ff23d1a51b1
Ali Kariapper <kariappa@mcmaster.ca>
2020-06-07 18:20:32 -0400

    Customization

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,24 @@
 import React,{ Component } from 'react';
 import CardList from './CardList'
-import { robots } from './robots'
 import SearchBox from './SearchBox'
-
+import './App.css'
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users}));
+
+            
+        
     }
 
     onSearchChange = (event) => {
@@ -22,13 +30,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+        
         return (
             <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f2">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots} />
             </div>
         );
+        }
     }
 
 }
