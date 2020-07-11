37b2d931e26391549a21b695e5de104ef1b510c2
Ali Kariapper <kariappa@mcmaster.ca>
2020-07-11 18:33:38 -0400

    Added Error handling

---
+++ robofriends/src/components/ErrorBoundary.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hasError: false
+        }
+    }
+
+
+componentDidCatch() {
+    this.setState({hasError: true})
+}
+
+    render() {
+        if (this.state.hasError) {
+            return(
+                <h1>Error Loading</h1>
+            )
+        }
+
+        return this.props.children
+
+    }
+}
+
+export default ErrorBoundary
--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import './App.css'
 import Scroll from '../components/Scroll'
+import ErrorBoundary from '../components/ErrorBoundary'
 
 class App extends React.Component {
     constructor() {
@@ -38,8 +39,10 @@ class App extends React.Component {
                     <h1 className='f1' >RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
+                        <ErrorBoundary>
                         <CardList robots={filteredRobots} />
-                    </Scroll>
+                        </ErrorBoundary>
+                   </Scroll>
                 </div>
         }
     }
